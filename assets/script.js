265c03cff41e720bf60ce9b9f2f93c79852f235c
unknown <mikenavarrete1998@gmail.com>
2023-12-04 17:59:23 -0500

    <changes made to the JavaScript console section of the prework-study-guide>

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
+
+function selectTopic() {
+ if (topics[0] === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (topics[0] === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (topics[0] === 'Git') {
+   console.log("Let's study Git!");
+ } else if (topics[0] === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
+}
+listTopics()
+selectTopic()
